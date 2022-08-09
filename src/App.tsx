f2ec49a9bf93753e8d182ea4c38310d33fb1c02a
Vitor <vitor.mancio@gmail.com>
2022-08-09 17:22:00 -0300

    create createServer on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
 import { Dashboard } from "./componets/Dashboard";
 import { Header } from "./componets/Header";
+import {createServer} from 'miragejs'
 import { GlobalStyle } from "./style/global";
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [ 
+        {
+          id:1,
+          title:'Transaction 1',
+          amount:400,
+          type:'deposit',
+          category:'Food',
+          createdAt: new Date()
+
+        }
+      ]
+
+    })
+  }
+}) 
 
 export function App() {
   return (
