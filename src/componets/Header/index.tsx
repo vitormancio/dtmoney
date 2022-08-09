f1355dd6cdb9602b4b63eeae13802c65d444d976
Vitor <vitor.mancio@gmail.com>
2022-08-09 05:56:14 -0300

    finish base of html of Header Component

--- src/componets/Header/index.tsx
+++ src/componets/Header/index.tsx
@@ -1,7 +1,12 @@
+import logoImg from '../../assets/logo.svg'
+
 export function Header () {
     return (
         <header>
-            Header
+            <img src={logoImg} alt="dt money" />
+            <button type='button'>
+                Nova Transação
+            </button>
         </header>
     )
 }
