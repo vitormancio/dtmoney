4b2f956db49bc74dc3bc8424e4440e79e67dc3f7
Vitor <vitor.mancio@gmail.com>
2022-08-09 05:32:26 -0300

    create glbal style

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { GlobalStyle } from "./style/global";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
+    
   );
 }
 
